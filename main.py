9ced209688963e77895d1dd68cd0ac01212da01c
Marcondes <marcondesgoncalves159@gmail.com>
2024-03-24 16:31:59 -0300

    Criando a HomePage

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+
+class HomePage(Screen):
+    pass
+
+
 
 # No arquivo kv está escrito a nossa interface
 GUI = Builder.load_file("main.kv")
-
 class MainApp(App):
 
     def build(self):
         return GUI
 
-
-
+    def on_start(self):
+        pass
 
 MainApp().run()
 
